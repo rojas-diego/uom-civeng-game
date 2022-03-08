abed01ee1421a8a832117ac10cbbef2f15e68a61
Diego ROJAS <63368264+rojasdiegopro@users.noreply.github.com>
2022-03-08 07:27:15 +0100

    feat: add basic closest neighbour algorithm

--- router.py
+++ router.py
@@ -1,16 +1,70 @@
 import random
 
 
+def distance_between(a, b):
+    """
+    Computes the euclidian distance between two locations.
+    """
+    return ((((b.x - a.x)**2) + ((b.y-a.y)**2))**0.5)
+
+
+def closest_not_connected_neighbour(loc, connections, locations):
+    """
+    This function attempts to find the closest location to `loc` which is not directory connected to it.
+    """
+
+    closest_neighbour_distance = 1_000_000_000  # We set this to almost infinity
+    closest_neighbour = None
+
+    for other_loc in locations:
+        # We don't want to connect a location with itself.
+        if loc == other_loc:
+            continue
+
+        # We make sure that we haven't already connected these two locations.
+        already_connected = False
+        for connection in connections:
+            print(connection[0], '-', connection[1])
+            if (connection[0] == loc and connection[1] == other_loc) or (connection[0] == other_loc and connection[1] == loc):
+                already_connected = True
+        if already_connected:
+            continue
+
+        # We compute the distance and see if it's closer than our best current guess.
+        distance = distance_between(loc, other_loc)
+        if distance < closest_neighbour_distance:
+            closest_neighbour = other_loc
+            closest_neighbour_distance = distance
+
+    return closest_neighbour
+
+
 def connect_locations(locations, connect):
     """
     You are given a list of locations that you must connect together using the `connect` function.
-    To check whether connecting two locations requires building a bridge, use the `need_bridge_to_connect` function.
     Calling connect multiple times with the same locations is idempotent.
     """
 
-    # Code to connect every point with each other.
-    # This solution is not optimal at all because it uses too much resources.
-    for loc_1 in locations:
-        for loc_2 in locations:
-            if random.randint(0, 4) == 0:
-                connect(loc_1, loc_2)
+    # We declare a list to store which locations we already connected.
+    connections = []
+
+    # For each location in the map.
+    for loc in locations:
+
+        # Find its closest neightbour with which it's not connected.
+        neighbour = closest_not_connected_neighbour(
+            loc, connections, locations)
+
+        # If we found no suitable neighbour we skip this location.
+        if neighbour == None:
+            continue
+
+        print(loc, "'s closest neighbour is ", neighbour, sep='')
+
+        # Connect them
+        connect(loc, neighbour)
+
+        # Store this new connection in our connections list
+        connections.append((loc, neighbour))
+
+        print('Connections count', len(connections))
